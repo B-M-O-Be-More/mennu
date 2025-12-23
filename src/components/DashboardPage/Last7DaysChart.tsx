800bdc7f57d30563eeb101a28af277f955ad37b9
VCalidor <victor.calidor@gmail.com>
2025-12-23 16:51:06 -0300

    feature/chart dos ultimos 7 dias

---
+++ src/components/DashboardPage/Last7DaysChart.tsx
@@ -0,0 +1,69 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+  TooltipItem
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip);
+
+export default function Last7DaysChart() {
+  const maxValue = 10;
+
+  const data = {
+    labels: ["Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"],
+    datasets: [
+      {
+        data: [2, 3, 1, 0, 4, 0, 2],
+        backgroundColor: "#FF3D00",
+        borderRadius: 6,
+        barThickness: 6,
+        stack: "progress",
+        grouped: false,
+      },
+      {
+        data: Array(7).fill(maxValue),
+        backgroundColor: "#E5E7EB",
+        borderRadius: 6,
+        barThickness: 10,
+        stack: "progress",
+        grouped: false,
+      },
+
+    ],
+  };
+
+  const options = {
+    indexAxis: "y" as const,
+    responsive: true,
+    plugins: {
+      legend: { display: false },
+      tooltip: {
+        enabled: true,
+        filter: function (tooltipItem: TooltipItem<"bar">) {
+          return tooltipItem.datasetIndex === 0;
+        },
+      },
+    },
+    scales: {
+      x: {
+        display: false,
+        max: maxValue,
+        stacked: false,
+      },
+      y: {
+        stacked: false,
+        grid: { display: false },
+        ticks: {
+          color: "#6A7282",
+          font: { size: 14 },
+        },
+      },
+    },
+  };
+
+  return <Bar data={data} options={options} />;
+}
--- src/components/DashboardPage/index.ts
+++ src/components/DashboardPage/index.ts
@@ -2,8 +2,9 @@ import { DashBoardPage } from "./component";
 import { DashBoardPageProps } from "./interface";
 import ModuleCard from "./moduleCard"
 import IconBox from "./iconBoxed"
+import Last7DaysChart from "./Last7DaysChart"
 
 export default DashBoardPage;
 export type { DashBoardPageProps };
-export { ModuleCard, IconBox }
+export { ModuleCard, IconBox, Last7DaysChart }
 
